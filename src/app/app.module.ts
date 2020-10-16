3c7aa4c6c82e5761210f282b8b758849aebbbd66
Jhballen <Jhballen@poligran.edu.co>
2020-10-16 05:55:44 -0500

    Primera version App y se generaron component

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PictureListComponent } from './picture-list/picture-list.component';
+import { PictureComponent } from './picture/picture.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PictureListComponent,
+    PictureComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
